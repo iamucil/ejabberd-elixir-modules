c38be5e09c7bf654a794a402b5074fedd966644b
iamucil <ecko.ucil@gmail.com>
2018-07-13 16:10:08 +0700

    add module filtering packet

---
+++ mod_presence_demo/lib/filter_packet_demo.ex
@@ -0,0 +1,21 @@
+defmodule FilterPacketDemo do
+  import Ejabberd.Logger
+  @behaviour :gen_mod
+
+  def start(_host, _opts) do
+    info('Starting ejabberd module Filter Packet Demo')
+    Ejabberd.Hooks.add(:filter_packet, :global, __ENV__.module, :on_filter_packet)
+    :ok
+  end
+
+  def stop(_host) do
+    info('Stoping ejabberd module Filter Packet Demo')
+    Ejabberd.Hooks.delete(:filter_packet, :global, __ENV__.module, :on_filter_packet)
+    :ok
+  end
+
+  def on_filter_packet({from, to, xml} = packet) do
+    info("Filtering packet: #{inpect {from, to, xml}}")
+    packet
+  end
+end
