ab97daf8409070baff117285a1b332149eb4bc01
Eko Susilo <ecko.ucil@gmail.com>
2018-07-15 17:23:21 +0700

    remove ejabberd logger

--- mod_filter_packet/lib/mod_filter_packet_ex.ex
+++ mod_filter_packet/lib/mod_filter_packet_ex.ex
@@ -3,19 +3,19 @@ defmodule ModFilterPacketEx do
   @behaviour :gen_mod
 
   def start(_host, _opts) do
-    info('Starting ejabberd module Filter Packet Demo')
+    # info('Starting ejabberd module Filter Packet Demo')
     Ejabberd.Hooks.add(:filter_packet, :global, __ENV__.module, :on_filter_packet)
     :ok
   end
 
   def stop(_host) do
-    info('Stoping ejabberd module Filter Packet Demo')
+    # info('Stoping ejabberd module Filter Packet Demo')
     Ejabberd.Hooks.delete(:filter_packet, :global, __ENV__.module, :on_filter_packet)
     :ok
   end
 
   def on_filter_packet({from, to, xml} = packet) do
-    info("Filtering packet: #{from}")
+    # info("Filtering packet: #{from}")
     packet
   end
 end
