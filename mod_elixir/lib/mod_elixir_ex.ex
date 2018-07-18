b8fc9e687b2fff7f6b630f78369e9288f6b1fd5c
iamucil <ecko.ucil@gmail.com>
2018-07-18 11:52:47 +0700

    remove unused variable on mod_elixir

--- mod_elixir/lib/mod_elixir_ex.ex
+++ mod_elixir/lib/mod_elixir_ex.ex
@@ -5,13 +5,13 @@ defmodule ModElixirEx do
     Ejabberd.Hooks.add(:set_presence_hook, host, __ENV__.module, :on_presence, 50)
     :ok
   end
-  
+
   def stop(host) do
     Ejabberd.Hooks.delete(:set_presence_hook, host, __ENV__.module, :on_presence, 50)
     :ok
   end
-  
-  def on_presence(user, _server, _resource, _packet) do
+
+  def on_presence(_user, _server, _resource, _packet) do
     :none
-  end 
+  end
 end
